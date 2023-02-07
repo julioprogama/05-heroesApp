6f054bc7e46c44d0b1a70506b12576e61cc5e105
julio cesar landazury castillo <83124737+julioprogama@users.noreply.github.com>
2023-02-07 12:14:22 -0500

    init

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-agregar',
@@ -7,5 +9,46 @@ import { Component } from '@angular/core';
   ]
 })
 export class AgregarComponent {
+  
+  @Input() heroes!: Heroe;
+  
+  id:string="";
+  superHero:string="";
+  alter_ego:string="";
+  publisher:string=" ";
+  first_appearance:string="";
+  characters:string="";
+  
+  private _historial !:Publisher;
+  public search : Gif[] = [];
+  
+  get historial(): string[]{return [...this._historial];}
+
+  constructor(private heroeService: HeroesService){ }
+  
+  get Publisher(){
+    
+  }
+
+  setHero(){
+    this.heroes.id = this.id;
+    this.heroes.alter_ego = this.alter_ego;
+    this.heroes.publisher = this.publisher;
+    this.heroes.characters = this.characters;
+
+  }
+
+  agregar(){
+    
+    if(!this.heroes){
+
+
+    }
+    this.heroeService.addHero(this.heroes).subscribe(
+      (hero:Heroe) => { this.heroes = hero; }
+    );
+
+    
+  } 
 
 }
